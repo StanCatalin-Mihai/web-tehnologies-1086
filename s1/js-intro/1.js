fc26be6ed3312e4bd8572c3a3f1576c225af38bb
StanCatalin-Mihai <stancatalin18@stud.ase.ro>
2020-10-06 13:20:44 +0300

    idk2

---
+++ s1/js-intro/1.js
@@ -0,0 +1,26 @@
+'use strict'
+let n = 5
+console.log(typeof n)
+let s = 'a string'
+console.log(typeof s)
+let b = true
+console.log(typeof b)
+
+
+console.log('types')
+
+let myVar = ' i am a Little string'
+console.log(typeof myVar)
+myVar = 5
+console.log(typeof myVar)
+
+let myOtherVar
+console.log(myOtherVar)
+console.log(typeof myOtherVar)
+
+let myObject = 
+{
+    name : 'some name',
+    age : 40
+}
+
---
+++ s1/js-intro/2.js
@@ -0,0 +1,14 @@
+let s = 'some string'
+
+console.log(typeof s)
+console.log(s.toUpperCase())
+
+let n1 = null
+console.log(n1)
+console.log(typeof n1)
+
+console.log('aaa' + 5)
+
+console.log (5 == '5')
+
+console.log (5 === '5')
